06a96aec922da0eaed5f30a50f21819c31cd8d5e
AkankshDeshmukh <akankshadeshmukh668@gmail.com>
2025-10-06 13:50:16 +0530

    first charatervalues

---
+++ charatervalues.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	/*
+	syntax:decalration datatype
+	     datatype varname;
+	     char ch1,ch2,ch3;
+	     int n1,n2=100(initialisation);
+	     float x,y;*/
+	     
+	     
+	char ch1='A',ch2,ch3;
+	printf("ch1=%c",ch1); 
+	ch2='1';
+	printf("ch2=%c\n",ch2);
+	ch3='@';
+	printf("ch3=%c\n");
+	printf("ch2=%c",ch3);    
+}
---
+++ first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+   
+ printf("Hello world\n");
+ printf("Welcome\\n");
+ printf("\n");
+ printf("How \nare you");
+ printf("fortune\t cloude");
+}
